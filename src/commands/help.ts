a0159cd8fe0d65d14855cd985c762c48baaf4eae
Alviss Raghnall <isiomastewart@gmail.com>
2025-09-23 23:56:51 +0100

    added buy command to help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,6 +6,7 @@ export function sendHelpMessage(ctx: Context) {
       "🆕 /generate - Generate a new Solana wallet\n" +
       "🔑 /import - Import wallet using private key or mnemonic\n" +
       "💰 /balance - Check balance of your wallets\n" +
+      "💰 /buy - Buy any arbitrary token\n" +
       "👛 /wallets - View all your stored wallets\n" +
       "⚙️ /settings - Bot settings\n" +
       "❓ /help - Show this help message",
